c103a1da9cfb3fe5da1f7e57fd18fb1a65c45875
Elmar KesslerElmar Kesslergit config --global user.name Elmar <elmar.kessler@noseryoung.com>
2024-01-08 14:13:13 +0100

    update

---
+++ SocialMediaPlattform/src/main/java/com/example/SocialMediaPlattform/SocialMediaPlattformApplication.java
@@ -0,0 +1,13 @@
+package com.example.SocialMediaPlattform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaPlattformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaPlattformApplication.class, args);
+	}
+
+}
